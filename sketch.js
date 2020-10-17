547ec2eaebf6d22319965759c56d76cbc2052d4c
Bryce Graves <brycegraves97@gmail.com>
2020-10-17 02:29:32 -0700

    Refactor the way populations are represented and further cleanup

--- sketch.js
+++ sketch.js
@@ -1,19 +1,24 @@
-// Zombulator homework help/experiementing
-// Zombulator boiler plate code provided by Elsie Charles of the class CS160 in fall 2017
+/*
+  Zombulator homework help/experiementing
+  Zombulator boiler plate code provided by Elsie Charles of the class CS160 in fall 2017
+*/
 
-// --TODO--
-// Path humans away from Zombies
-// Path humans together
-// Path zombies towards humans
+/*
+  TODO: Path humans away from Zombies
+  TODO: Path humans together
+  TODO: Path zombies towards humans
+*/
 
 const MIN_SIZE = 10;
 const MAX_SIZE = 50;
 const POPULATION_SIZE = 500;
 
-const population = [];
+const population = {
+  human: [],
+  zombie: [],
+};
+
 let backgroundColor;
-let zombieCount = 0;
-let humanCount = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -31,92 +36,82 @@ function draw() {
 
 const initializePopulation = () => {
   for (let i = 0; i < POPULATION_SIZE; ++i) {
-    const humanoid_type = random(0, 100);
+    const current = random(0, 100) < 50 ? initializeHuman() : initializeZombie();
 
-    if (humanoid_type <= 50) {
-      population.push(initializeZombie());
-      ++zombieCount;
-    } else {
-      population.push(initializeHuman());
-      ++humanCount;
-    }
+    population[current.humanoidType].push(current);
   }
 };
 
-function handleCollisions() {
-  for (let i = 0; i < POPULATION_SIZE; ++i) {
-    for (let j = i + 1; j < POPULATION_SIZE; ++j) {
-      let attacker = population[i];
-      let target = population[j];
-
-      if (isTouching(attacker, target)) {
-        if (attacker.size != 0 && target.size != 0) {
-          print('Fight');
-          fight(attacker, target);
-        }
+const handleCollisions = () => {
+  population.human.forEach((human) => {
+    population.zombie.forEach((zombie) => {
+      if (isTouching(human, zombie)) {
+        fight(human, zombie);
       }
-    }
-  }
-}
+    });
+  });
+};
 
 const drawPopulationCounts = () => {
   stroke(0);
   textSize(72);
   textAlign(CENTER);
-  text('Zombies: ' + zombieCount, width / 2, 100);
-  text('Humans: ' + humanCount, width / 2, height - 100);
+  text('Zombies: ' + population.zombie.length, width / 2, 100);
+  text('Humans: ' + population.human.length, width / 2, height - 100);
 };
 
 const handlePopulation = () => {
-  for (let i = 0; i < POPULATION_SIZE; ++i) {
+  [...population.human, ...population.zombie].forEach((current) => {
     const direction = random(0, 100);
 
-    fill(population[i].color);
-    ellipse(population[i].x, population[i].y, population[i].size, population[i].size);
+    fill(current.color);
+    ellipse(current.x, current.y, current.size, current.size);
 
     if (direction < 20) {
-      population[i].x += population[i].speed;
+      current.x += current.speed;
     } else if (direction < 40) {
-      population[i].x -= population[i].speed;
+      current.x -= current.speed;
     } else if (direction < 60) {
-      population[i].y += population[i].speed;
+      current.y += current.speed;
     } else {
-      population[i].y -= population[i].speed;
+      current.y -= current.speed;
     }
 
-    if (population[i].x >= windowWidth) {
-      population[i].x -= population[i].speed;
-    } else if (population[i].x <= 0) {
-      population[i].x += population[i].speed;
+    if (current.x >= windowWidth) {
+      current.x -= current.speed;
+    } else if (current.x <= 0) {
+      current.x += current.speed;
     }
 
-    if (population[i].y >= windowHeight) {
-      population[i].y -= population[i].speed;
-    } else if (population[i].y <= 0) {
-      population[i].y += population[i].speed;
+    if (current.y >= windowHeight) {
+      current.y -= current.speed;
+    } else if (current.y <= 0) {
+      current.y += current.speed;
     }
-  }
+  });
 };
 
 const initializeZombie = () => {
   return {
+    id: uuid(),
     humanoidType: 'zombie',
     x: random(0, windowWidth),
     y: random(0, 200),
     speed: random(0.25, 3),
     size: random(MIN_SIZE, MAX_SIZE),
-    color: color(random(190, 255), random(50, 150), random(50, 150)),
+    color: color(random(150, 250), random(50, 150), random(50, 150)),
   };
 };
 
 const initializeHuman = () => {
   return {
+    id: uuid(),
     humanoidType: 'human',
     x: random(0, windowWidth),
     y: random(windowHeight - 200, windowHeight),
     speed: random(0.25, 3),
     size: random(MIN_SIZE, MAX_SIZE),
-    color: color(random(50, 150), random(50, 150), random(190, 255)),
+    color: color(random(50, 150), random(50, 150), random(150, 250)),
   };
 };
 
@@ -125,31 +120,23 @@ const isTouching = (self, target) =>
     ? dist(self.x, self.y, target.x, target.y) <= self.size / 2 + target.size / 2
     : false;
 
-const fight = (attacker, target) => {
-  if (target.size < attacker.size) {
-    if (target.humanoidType == 'zombie') {
-      target.size = 0;
-      zombieCount--;
-    } else {
-      target.humanoidType = 'zombie';
-      target.color = color(random(190, 255), random(50, 150), random(50, 150));
-      humanCount--;
-      zombieCount++;
-    }
-  } else if (target.size > attacker.size) {
-    if (attacker.humanoidType == 'zombie') {
-      attacker.size = 0;
-      zombieCount--;
-    } else {
-      attacker.humanoidType = 'zombie';
-      attacker.color = color(random(190, 255), random(50, 150), random(50, 150));
-      humanCount--;
-      zombieCount++;
-    }
-  } else if (target.size === attacker.size) {
-    target.size = 0;
-    attacker.size = 0;
-    zombieCount--;
-    humanCount--;
+const fight = (human, zombie) => {
+  if (human.size < zombie.size) {
+    const newZombie = human;
+    newZombie.humanoidType = 'zombie';
+    newZombie.color = color(random(190, 255), random(50, 150), random(50, 150));
+
+    population.human = population.human.filter((h) => h.id !== newZombie.id);
+    population.zombie.push(newZombie);
+  } else if (human.size > zombie.size) {
+    population.zombie = population.zombie.filter((z) => z.id !== zombie.id);
+  } else {
+    population.human = population.human.filter((h) => h.id !== human.id);
+    population.zombie = population.zombie.filter((z) => z.id !== zombie.id);
   }
 };
+
+const uuid = () =>
+  ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
+  );
